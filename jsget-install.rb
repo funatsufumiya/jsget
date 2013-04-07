55a2d541050e38f382207a3965c44d8d8ebf014d
atmarksharp <funatsu.fumiya@gmail.com>
2013-04-07 23:59:23 +0900

    Update jsget-install.rb

--- jsget-install.rb
+++ jsget-install.rb
@@ -12,5 +12,9 @@ open(url) do |source|
   end
 end
 
+puts "download 'https://raw.github.com/atmarksharp/jsget/master/jsget'"
+
 FileUtils.move(filename, '/usr/bin/jsget',{:force => true})
+puts "installing '/usr/bin/jsget'"
 FileUtils.chmod(0755, '/usr/bin/jsget', {:verbose => true})
+puts "install successed."
