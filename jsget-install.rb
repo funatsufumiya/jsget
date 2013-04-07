9eea651bbb148b3d726ec28cfa589ac78623010f
atmarksharp <funatsu.fumiya@gmail.com>
2013-04-08 00:55:09 +0900

    Update jsget-install.rb

--- jsget-install.rb
+++ jsget-install.rb
@@ -4,6 +4,9 @@ require 'fileutils'
 @red = "\e\[00;31m"
 @green = "\e\[00;32m"
 @clear = "\e\[00;0m"
+@yellow = "\e\[01;33m"
+@lightgreen = "\e\[01;32m"
+@lightblue = "\e\[01;34m"
 
 def console(s)
   puts "#{@green}=====> #{@clear}" + s
@@ -19,12 +22,12 @@ open(url) do |source|
   end
 end
 puts
-console("download 'https://raw.github.com/atmarksharp/jsget/master/jsget'")
+console("#{@lightgreen}[download]#{@clear} 'https://raw.github.com/atmarksharp/jsget/master/jsget'")
 FileUtils.move(filename, '/usr/bin/jsget',{:force => true})
-console("installing '/usr/bin/jsget'")
+console("#{@lightblue}[install]#{@clear} '/usr/bin/jsget'")
 FileUtils.chmod(0755, '/usr/bin/jsget', {:verbose => true})
-console("createing folder '~/.jsget'")
+console("#{@lightblue}[create directory]#{@clear} '~/.jsget'")
 FileUtils.mkdir_p("#{ENV["HOME"]}/.jsget/")
 FileUtils.chmod(0775, "#{ENV["HOME"]}/.jsget/", {:verbose => true})
-console("install successed :)")
+console("#{@yellow}install successed :){@clear}")
 puts
